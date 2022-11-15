19e43df46d866e7a1232b30b80def10e8d6793eb
DevSciCloan <116118420+DevSciCloan@users.noreply.github.com>
2022-11-15 12:00:14 -0700

    Updated Portfolio
    Adding some UX updates to site navigation.

--- js/navbar.js
+++ js/navbar.js
@@ -12,3 +12,26 @@ for (var i = 0; i < btns.length; i++) {
   this.className += " active";
   });
 }
+
+function openNav() {
+  document.getElementById("mySidenav").style.display = "block";
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.display = "none";
+  document.getElementById("main").style.marginLeft= "0";
+}
+
+
+window.onscroll = function() {
+  if (document.documentElement.scrollTop < 80) {
+    document.getElementById("navid").style.display = 'block';
+    document.getElementById('mini-nav').style.display = 'none';
+    closeNav();
+  } else {
+    document.getElementById("navid").style.display = "none";
+    document.getElementById('mini-nav').style.display = 'block';
+  }
+}
